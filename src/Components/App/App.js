326fd043053add849ed2a2697c693a94f5cde6c6
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-19 14:49:13 -0400

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <div>Sweet and Sour Horoscope</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
