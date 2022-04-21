dc11ab5b624b7170fd3c5b1104a2943d078f3a47
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 17:18:46 -0400

    Add in the rendering for the header with title.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Header.css"
+
+const Header = () => {
+  return(
+    <header className="header">
+      <div className="title">Mycologist Horoscope</div>
+    </header>
+
+  )
+}
+
+
+export default Header
