08ad11f5bebfa8b179b40f24df318cc51d264766
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 17:59:59 -0400

    Add CreateNewHoroscope form component to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Form from "../Form/Form";
+import CreateHoroscopeForm from "../Form/CreateHoroscopeForm";
 import getHoroscopeData from "../../apiCalls";
 
 class App extends Component {
@@ -17,17 +18,22 @@ componentDidMount = () => {
   .then(data => this.setState({ horoscopes: data}))
 }
 
+getHoroscope = () => {
+  this.setState({ horoscopes: [...this.state.horoscopes ]});
+  console.log("FUCK")
+}
 
-// addHoroscope = (newHoroscope) {
-// this.setState({ horoscopes: [...this.state.horoscopes, newHoroscope] });
-// console.log("Horoscopes", this.state.horoscope)
-//  }
+addHoroscope = (newHoroscope) => {
+  this.setState({ horoscopes: [...this.state.horoscopes, newHoroscope] });
+  console.log("Horoscopes", this.state.horoscope)
+}
 
 render() {
   return (
     <div className="App">
     <Header />
     <Form addHoroscope={this.state.horoscopes}/>
+    <CreateHoroscopeForm />
     </div>
   );
 }
