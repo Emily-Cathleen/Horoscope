10fe776a8cb10c97e9cd3e90f649a81ea6fa57f8
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 17:17:14 -0400

    Add in the Header and form rendering to APP

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "../Header/Header";
+import Form from "../Form/Form";
+import getHoroscopeData from "../../apiCalls";
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      horoscopes: []
+    }
+  }
+
+componentDidMount = () => {
+  getHoroscopeData()
+  .then(data => this.setState({ horoscopes: data}))
+}
+
+
+// addHoroscope = (newHoroscope) {
+// this.setState({ horoscopes: [...this.state.horoscopes, newHoroscope] });
+// console.log("Horoscopes", this.state.horoscope)
+//  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-    <div>Sweet and Sour Horoscope</div>
-      </header>
+    <Header />
+    <Form addHoroscope={this.state.horoscopes}/>
     </div>
   );
 }
 
+}
 export default App;
