308e405a9f80e07fa820e67b3efbf6c539c5e1bf
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 18:01:13 -0400

    Add in basic imports and return for a SingleHoroscope

---
+++ src/Components/SingleHoroscope/SingleHoroscope.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import "./SingleHoroscope.css"
+
+const SingleHoroscope = () => {
+  return(
+    <header className="">
+      <div className="title">Mycologist Horoscope</div>
+    </header>
+
+  )
+}
+
+
+export default Header
