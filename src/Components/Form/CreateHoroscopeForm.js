60b4d3d830b5ba03d25945843e052a2b34c3f5bf
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 18:00:28 -0400

    Added submitNewHoroscopeButton method to correct form component

--- src/Components/Form/CreateHoroscopeForm.js
+++ src/Components/Form/CreateHoroscopeForm.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import './Form.css';
 
 class CreateHoroscopeForm extends Component {
@@ -14,25 +12,38 @@ class CreateHoroscopeForm extends Component {
     }
   }
 
+clearInputs = () => {
+  this.setState({ month: '', sign: '', messages: '' });
+    }
+
+handleChange = (event) => {
+  this.setState({ [event.target.name]: event.target.value })
+  }
+
+submitNewHoroscopeButton = (event) => {
+  event.preventDefault();
+      const newHoroscope = { ...this.state }
+      this.props.addHoroscope(newHoroscope);
+      this.clearInputs();
+  }
+
   render() {
     return (
       <form className = "form-container" >
-      <section className="right-box">
-      <div className="add-your-own-horoscope">Add your own horoscope</div>
-      <input className="input-field"
-          type='text'
-          placeholder='Enter your Horoscope Here'
-          name='messages'
-          value={this.state.messages}
-          onChange={event => this.handleChange(event)}
-        />
-      <button onClick={(event) => this.addHoroscope(event)}>Add Your Horoscope</button>
-      </section>
+        <section className="right-box">
+          <div className="add-your-own-horoscope">Add your own horoscope</div>
+          <input className="input-field"
+              type='text'
+              placeholder='Enter your Horoscope Here'
+              name='messages'
+              value={this.state.messages}
+              onChange={event => this.handleChange(event)}
+            />
+          <button onClick={(event) => this.addHoroscope(event)}>Add Your Horoscope</button>
+        </section>
       </form>
     )
-    )
-
-}
+  }
 }
 
   export default CreateHoroscopeForm
