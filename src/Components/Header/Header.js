f4f575696c4d6d4eb7023764d8794940b3d788ec
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-23 16:35:14 -0400

    Add a home button to my header to redirect from singleHoroscope page to home.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from "react"
 import "./Header.css"
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+
+const Header = ({ resetHome }) => {
   return(
     <header className="header">
       <div className="title">Mycologist Horoscope</div>
+      <Link to="/">
+        <button className="home-button" onClick={() => {resetHome()}}>HOME</button>
+      </Link>
     </header>
 
   )
