97311ed412a1fda99460a485f460bf0474223964
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 14:51:18 -0400

    Create savedCard as a component to be displayed on the saved card page.

---
+++ src/Components/SavedHoroscopes/SavedCard.js
@@ -0,0 +1,22 @@
+import "./SavedCard.css";
+
+const SavedCard = ({ userHoroscope }) => {
+  return(
+
+    <div className="saved-card-contianer">
+      <img className="mushroom-image" src={userHoroscope.img_url} alt="mushroom picture"/>
+      <div className="horoscope-details-container">
+        <div className="sign">
+          {userHoroscope.sign}
+        </div>
+        <div className="mushroom-name">
+          {userHoroscope.name}
+        </div>
+      </div>
+    </div>
+
+  )
+
+}
+
+export default SavedCard
