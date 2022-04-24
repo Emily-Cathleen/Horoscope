86c28bf9ee0e0f5b888e47ad5d0b1cbaef9dc691
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 15:12:14 -0400

    Add tests for header, buttons, and form on main page

--- cypress/integration/home_page.js
+++ cypress/integration/home_page.js
@@ -7,5 +7,18 @@ describe('Mycology horoscope main page', () => {
 //Header
 it('Should see a Header', () => {
   cy.visit("http://localhost:3000/")
-  .get(".header")
+  .get(".header").contains('Mycologist Horoscope')
 });
+
+it('Should see a saved Horoscopes Page button', () => {
+  cy.visit("http://localhost:3000/")
+  .get(".favorite")
+  .click()
+  .get('.sorted-horoscopes-container').contains('Here are all of your saved horoscopes:')
+  .should("exist")
+});
+
+it('Should see a "Choose your birthday form', () => {
+  cy.visit("http://localhost:3000/")
+  .get(".form-container").contains('Select your birthday...')
+})
