5fabfdcd02baa140be892bc631aae1100789e4e5
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 12:31:47 -0400

    Link to "Learn More" Button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,13 +6,15 @@ import { Link } from 'react-router-dom';
 const Header = ({ resetHome }) => {
   return(
     <header className="header">
-      <div className="title">Mycologist Horoscope</div>
-      <Link to="/">
-        <button className="home-button" onClick={() => {resetHome()}}>HOME</button>
-      </Link>
-      <Link to="/learn">
-        <button className="learn-more-button" onClick={() => {resetHome()}}>Learn More</button>
-      </Link>
+      <h1 className="title">Mycologist Horoscope</h1>
+      <div className="button-container">
+        <Link to="/">
+          <button className="home-button" onClick={() => {resetHome()}}>HOME</button>
+        </Link>
+        <Link to="/learn">
+          <button className="learn-more-button" onClick={() => {resetHome()}}>Learn More</button>
+        </Link>
+      </div>
     </header>
 
   )
