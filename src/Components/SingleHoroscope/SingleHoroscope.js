6c04ae433d9a3ec8edbd783c7d56180991caba59
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 18:21:37 -0400

    Add in more info to single horoscope.

--- src/Components/SingleHoroscope/SingleHoroscope.js
+++ src/Components/SingleHoroscope/SingleHoroscope.js
@@ -7,18 +7,20 @@ import Header from "../Header/Header";
 const SingleHoroscope = ( { userHoroscope, saveHoroscope } ) => {
 
   return(
+    <div>
+    <div className="greeting"> {`Your sign is ${userHoroscope.sign}, happy forraging! `} </div>
     <div className="single-horoscope-container">
-    <div className="greeting"> {`Your sign is ${userHoroscope.sign}, happy shrooming! `} </div>
       <img className="mushroom-image" src={userHoroscope.img_url} alt="mushroom picture"/>
       <div className="horoscope-details-container">
         <div className="sign">
-          {userHoroscope.sign}
+          {`You are a beautiful ${userHoroscope.sign} and you will be ${userHoroscope.messages}`}
         </div>
         <div className="mushroom-name">
-          {userHoroscope.name}
+          {`Your mushroom is a ${userHoroscope.name}`}
         </div>
         <button className="favorite-button" onClick={() => {saveHoroscope(userHoroscope)}}>SAVE</button>
       </div>
+    </div>
     </div>
   )
 }
