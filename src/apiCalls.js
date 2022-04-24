bebe13563fb81fe42dc96112567e4990e889e8fc
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 17:01:31 -0400

    Deleted console.log

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,6 @@
 const getHoroscopeData = () => {
   return fetch('https://mycology-horoscope-api.herokuapp.com/v1/monthlyData')
   .then(response => response.json())
-  // .then(data => console.log(data))
 }
 
 export default getHoroscopeData;
