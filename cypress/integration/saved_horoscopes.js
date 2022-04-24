7f010de453dda49180efd52f3d2cfd76699c79da
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 17:10:43 -0400

    Add testing for the saved horosopes page

---
+++ cypress/integration/saved_horoscopes.js
@@ -0,0 +1,51 @@
+describe('The saved horoscopes page', () => {
+
+  let testData;
+
+  beforeEach(() => {
+    testData = [{
+      name: "Oyster Mushroom",
+      img_url: "https://upload.wikimedia.org/wikipedia/commons/9/94/Oyster_mushoom_fells.jpg",
+      id: 1,
+      month: [
+        "December",
+        "January"
+      ],
+      sign: "Capricorn",
+      messages: [
+        "hello Janurary Capricorn ",
+        "Hi January"
+      ]
+    }]
+  });
+
+  it('Should see a Header', () => {
+    cy.visit("http://localhost:3000")
+      cy.intercept("GET", 'https://mycology-horoscope-api.herokuapp.com/v1/monthlyData/', testData).as("horoscope")
+      .get(".header").contains('Mycologist Horoscope')
+  });
+
+  it('Should see a "Choose your birthday form', () => {
+    cy.visit("http://localhost:3000/")
+    .get(".form-container").contains('Select your birthday...')
+  });
+
+  it('Should fill out the form to see a single horoscope', () => {
+    cy.visit("http://localhost:3000");
+      cy.intercept("GET", 'https://mycology-horoscope-api.herokuapp.com/v1/monthlyData/', testData).as("horoscope")
+      .get('.month-selector')
+      .select("December")
+      .get('.day-selector')
+      .select(1)
+      .get('.submit-button')
+      .click()
+      .get('.single-horoscope-container')
+      .contains('Your sign is')
+      .get('.favorite-button')
+      .click()
+      .get('.saved-horoscopes-button')
+      .click()
+      .get('.sorted-horoscopes-container')
+  });
+
+  });
