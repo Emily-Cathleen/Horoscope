33c441fbd20b3f6ed095dedf1d5f6fc71bdf754b
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-24 18:18:53 -0400

    Delete the CreateHoroscopeForm component - this will be for a future iteration

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Form from "../Form/Form";
-import CreateHoroscopeForm from "../Form/CreateHoroscopeForm";
 import SingleHoroscope from "../SingleHoroscope/SingleHoroscope";
 import LearnMore from "../LearnMore/LearnMore";
 import SavedHoroscopes from "../SavedHoroscopes/SavedHoroscopes";
@@ -33,11 +32,6 @@ class App extends Component {
     this.setState({ userHoroscope: foundHoroscope })
   }
 
-  // addHoroscope = (newHoroscope) => {
-  //   this.setState({ horoscopes: [...this.state.horoscopes, newHoroscope] });
-  //   console.log("Horoscopes", this.state.horoscope)
-  // }
-
   saveHoroscope = (favoritedHoroscope) => {
     const filteredThing = this.state.favorites.filter(favorite => favorite.sign === favoritedHoroscope.sign)
     !filteredThing[0] && this.setState({ favorites: [...this.state.favorites, favoritedHoroscope ]});
@@ -56,7 +50,6 @@ render() {
     <Header resetHome={this.resetHome}/>
     <Route exact path="/" >
           <Form getHoroscope={this.getHoroscope}/>
-          <CreateHoroscopeForm addHoroscope={this.state.horoscopes}/>
     </Route>
     <Route exact path="/horoscope">
       {this.state.userHoroscope === null ? <Redirect to="/" /> : <SingleHoroscope userHoroscope={this.state.userHoroscope} saveHoroscope={this.saveHoroscope}/> }
@@ -64,7 +57,6 @@ render() {
     <Route exact path="/saved">
       <SavedHoroscopes favorites={this.state.favorites} />
     </Route>
-
     </div>
   );
 }
